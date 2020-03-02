958bf6490273c1bbf7899dad4489015e182be0dd
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-03-02 16:41:06 +0700

    add auto open browser when app init done

--- src/main/java/com/nittsu/kinjirou/UniversalApplication.java
+++ src/main/java/com/nittsu/kinjirou/UniversalApplication.java
@@ -1,5 +1,6 @@
 package com.nittsu.kinjirou;
 
+import java.io.IOException;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
@@ -7,11 +8,14 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = { UniversalApplication.class })
-public class UniversalApplication {
-
+public class UniversalApplication implements ApplicationListener<ApplicationReadyEvent> {
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
@@ -19,5 +23,19 @@ public class UniversalApplication {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(UniversalApplication.class, args);
+	}
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		Runtime rt = Runtime.getRuntime();
+		ConfigurableApplicationContext context = event.getApplicationContext();
+		Environment environment = context.getBean(Environment.class);
+		Integer port = environment.getProperty("server.port", Integer.class, 8080);
+
+		try {
+			rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:" + port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }

39bae6ed6ba7477d5aa9f9181462fc4e1fbbe361
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-03-30 16:21:45 +0700

    update file controller (add secure)

--- src/main/java/com/nittsu/kinjirou/core/config/WebSecurityConfig.java
+++ src/main/java/com/nittsu/kinjirou/core/config/WebSecurityConfig.java
@@ -99,7 +99,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        List<String> permitAllEndpointList = Arrays.asList(AUTHENTICATION_URL, REFRESH_TOKEN_URL, "/api/file/download/**");
+        List<String> permitAllEndpointList = Arrays.asList(AUTHENTICATION_URL, REFRESH_TOKEN_URL);
         Class<UsernamePasswordAuthenticationFilter> simpleAuthFilter = UsernamePasswordAuthenticationFilter.class;
 
         http // chain
--- src/main/java/com/nittsu/kinjirou/update/controllers/FileController.java
+++ src/main/java/com/nittsu/kinjirou/update/controllers/FileController.java
@@ -1,21 +1,18 @@
 package com.nittsu.kinjirou.update.controllers;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping({ "/file" })
 public class FileController {
-    // @Secured({ "ROLE_ADMIN" })
-    // @ResponseBody
+    @Secured({ "ROLE_ADMIN" })
     @RequestMapping(value = "/download/{fileName}", method = { RequestMethod.GET, RequestMethod.POST })
     public FileSystemResource downloadFile(HttpServletResponse response, @PathVariable("fileName") String fileName) {
         response.setContentType("application/x-zip");
